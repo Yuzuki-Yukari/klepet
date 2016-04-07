85ef718b5e4f782f6a540d7a939c0bf8927b2cda
Yuzuki Yukari <ar7012@student.uni-lj.si>
2016-04-07 17:45:20 +0000

    Zaključek dela na IV. nalogi

--- public/js/klepet.js
+++ public/js/klepet.js
@@ -43,10 +43,23 @@ Klepet.prototype.procesirajUkaz = function(ukaz) {
         sporocilo = 'Neznan ukaz';
       }
       break;
+    case 'dregljaj':
+      besede.shift();
+      var besedilo = besede.join(' ');
+      var parametri = besedilo.split('\"');
+      if(besede)
+      {
+        this.socket.emit('dregljaj', { vzdevek: parametri[0]});
+        sporocilo = 'Dregljaj za ' + parametri[0];
+      }
+      else
+      {
+        sporocilo = 'Neznan ukaz';
+      }
+      break;
     default:
       sporocilo = 'Neznan ukaz.';
       break;
-  };
-
+  }
   return sporocilo;
 };
